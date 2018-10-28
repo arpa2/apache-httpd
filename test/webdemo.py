cb65e729507f99e68e5653faa633abbc82d373b1
root <root@182c43decb54>
2018-10-28 17:12:34 +0000

    Python test program webdemo.py

---
+++ test/webdemo.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import sys
+import socket
+
+sys.path.append ('.')
+import tlspool
+
+
+sox = socket.socket (socket.AF_INET6, socket.SOCK_STREAM)
+website = 'localhost'
+sox.connect ( (website, 80) )
+
+cli2srv = ( tlspool.PIOF_STARTTLS_LOCALROLE_CLIENT | tlspool.PIOF_STARTTLS_REMOTEROLE_SERVER )
+cnx = tlspool.Connection (sox, service='http', flags=cli2srv)
+
+cnx.tlsdata.localid='testcli@tlspool.arpa2.lab'
+cnx.tlsdata.remoteid='testsrv@tlspool.arpa2.lab'
+
+web = cnx.starttls ()
+
+web.send ('GET / HTTP/1.0\r\nHost: ' + website + '\r\n\r\n')
+
+dta = web.recv (4096)
+while dta != '':
+	sys.stdout.write (dta)
+	dta = web.recv (4096)
+
+cnx.close ()
+
